4ac9939eaed76e1fd5c92a209c1b8272749c326e
garg-7 <garg.7@iitj.ac.in>
2021-04-20 15:02:53 +0530

    solved left recursion removal

--- A5/sol.c
+++ A5/sol.c
@@ -14,6 +14,7 @@ typedef struct set
     int max_size;
 } set;
 
+
 bool isEmpty(set *s)
 {
     if (strcmp(s->values[0], "") != 0)
@@ -105,12 +106,12 @@ void printSet(set *s)
         printf("EMPTY SET.\n");
     else
     {
-        printf("{");
+        printf("{\n");
         for (int i = 0; i < s->max_size; i++)
         {
             if (strcmp(s->values[i], "") != 0)
             {
-                printf(" [%s] ", s->values[i]);
+                printf("  [%s]\n", s->values[i]);
             }
         }
         printf("}\n");
@@ -118,13 +119,15 @@ void printSet(set *s)
     return;
 }
 
+
+
 void addSet(set *dst, set src){
-
+    int srcSize = getSize(&src);
+    for (int i=0;i<srcSize;i++){
+        addElement(dst, src.values[i]);
+    }
 }
 
-set cleanImmLR(set *s){
-
-}
 
 // !! IMPORTANT: source must be NULL-terminated. !!
 int getToken(char *source, char *tok, int *loc)
@@ -176,24 +179,176 @@ int getToken(char *source, char *tok, int *loc)
     return -1;
 }
 
-bool isFirstDone(char *nTerm, int *pDone, char **p, int n)
-{
-    char *token = (char *)malloc(MAX_UNIT_SIZE * sizeof(char));
-    for (int i = 0; i < n; i++)
-    {
+void getRecursive(char ** dst, set *s, int *count){
+    int curr = 0;
+    for(int i=0;i<getSize(s);i++){
         int counter = 0;
-        getToken(p[i], token, &counter);
-        if (strcmp(token, nTerm) == 0)
-        {
-            if (pDone[i] != 1)
-            {
-                free(token);
-                return false;
-            }
+        char tmp1[MAX_UNIT_SIZE];
+        char tmp2[MAX_UNIT_SIZE];
+        getToken(s->values[i], tmp1, &counter);
+        getToken(s->values[i], tmp2, &counter);
+        if (strcmp(tmp1, tmp2)==0){
+            // if the head is the same as the first 
+            // on the RHS
+            strcpy(dst[curr], s->values[i]);
+            curr+=1;
         }
     }
-    free(token);
-    return true;
+    *count = curr;
+}
+
+void getNonRecursive(char ** dst, set *s, int* count){
+    int curr = 0;
+    for(int i=0;i<getSize(s);i++){
+        int counter = 0;
+        char tmp1[MAX_UNIT_SIZE];
+        char tmp2[MAX_UNIT_SIZE];
+        getToken(s->values[i], tmp1, &counter);
+        getToken(s->values[i], tmp2, &counter);
+        if (strcmp(tmp1, tmp2)!=0){
+            // if the head is NOT the same as the first 
+            // on the RHS
+            strcpy(dst[curr], s->values[i]);
+            curr+=1;
+        }
+    }
+    *count = curr;
+}
+
+void getHead(char* dst, char* src){
+    int counter = 0;
+    getToken(src, dst, &counter);
+    return;
+}
+
+void getBody(char* dst, char* src){
+    int counter = 0;
+    char *tmp = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+    getToken(src, tmp, &counter);
+    while(getToken(src, tmp, &counter)!=-1){
+        strcat(dst, tmp);
+        strcat(dst, " ");
+    }
+    if (strlen(dst)>0){
+        dst[strlen(dst)-1] = 0; // remove the extra space at the end
+    }
+    return;
+}
+
+set cleanImmLR(set *s){
+
+    printf("[INFO] Asked to remove immediate LR from the productions:\n");
+    printSet(s);
+    // remove immediate left recursion from 
+    // the given set of productions and 
+    // return the new set of productions
+    set s1;
+    s1.max_size = 2*s->max_size;
+    s1.values = (char**)malloc(s1.max_size*sizeof(char*));
+    for(int i=0;i<s1.max_size;i++){
+        s1.values[i] = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+    }
+
+    int counter = 0;
+    char *head = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+    getToken(s->values[0], head, &counter);
+    printf("[INFO] Head of all the passed productions: [%s]\n", head);
+
+    // separate out the recursive and non-recursive
+    // productions for future ease.
+
+    char ** rec = (char**)malloc(s->max_size*sizeof(char*));
+    char ** nonRec = (char**)malloc(s->max_size*sizeof(char*));
+    for(int i=0;i<getSize(s);i++){
+        rec[i] = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+        nonRec[i] = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+        rec[i][0] = 0;
+        nonRec[i][0] = 0;
+    }
+    int *rsize = (int*)malloc(sizeof(int));
+    *rsize = 0;
+    int *nrsize = (int*)malloc(sizeof(int));
+    *nrsize = 0;
+
+    getRecursive(rec, s, rsize);
+    getNonRecursive(nonRec, s, nrsize);
+
+    printf("[INFO] Set of recursive productions: %d\n", *rsize);
+    for(int i=0;i<*rsize;i++)
+        printf("[INFO]   %s\n", rec[i]);
+    printf("[INFO] Set of non-recursive productions: %d\n", *nrsize);
+    for(int i=0;i<*nrsize;i++)
+        printf("[INFO]   %s\n", nonRec[i]);
+    // get the head of the first production in the passed set.
+    // note that the head should be same for all the productions
+    // in the set passed to this function.
+    
+    if (*rsize==0){
+        printf("[INFO] No immediate LR found.\n");
+        addSet(&s1, *s);
+        printf("[INFO] Set being returned:\n");
+        printSet(&s1);
+        return s1;
+    }
+
+    for (int i=0;i<*rsize;i++)
+    {
+        char *tmp = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+        char *body = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+        getBody(tmp, rec[i]);
+        getBody(body, tmp);
+      
+        char *toBeAdded = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+        strcat(toBeAdded, head);
+        strcat(toBeAdded, "' ");
+        if (strcmp(body, "?")==0)
+        ;
+        else{
+            strcat(toBeAdded, body);
+            strcat(toBeAdded, " ");
+        }
+        strcat(toBeAdded, head);
+        strcat(toBeAdded, "'");
+        printf("[INFO] Production being added: [%s]\n", toBeAdded);
+        addElement(&s1, toBeAdded);
+        // free(body);
+        // free(toBeAdded);
+        // free(tmp);
+    }
+
+    printf("[INFO] Non-recursive case.\n");
+    for(int i=0;i<*nrsize;i++){
+        char *body = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+        getBody(body, nonRec[i]);
+        if (strcmp(body, "?")==0)
+            continue;
+
+        char *toBeAdded = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+        strcat(toBeAdded, head);
+        strcat(toBeAdded, " ");
+        if (strcmp(body, "?")==0)
+        ;
+        else{
+            strcat(toBeAdded, body);
+            strcat(toBeAdded, " ");
+        }
+        strcat(toBeAdded, head);
+        strcat(toBeAdded, "'");
+        printf("[INFO] Production being added: [%s]\n", toBeAdded);
+        addElement(&s1, toBeAdded);
+        // free(body);
+        // free(toBeAdded);
+    }
+
+    // add the epsilon production
+    char* toBeAdded = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+    strcat(toBeAdded, head);
+    strcat(toBeAdded, "' ");
+    strcat(toBeAdded, "?");
+    addElement(&s1, toBeAdded);
+    free(toBeAdded);
+
+    return s1;
 }
 
 int main()
@@ -300,79 +455,111 @@ int main()
     int numTerminals = getSize(&terminals);
     int numNonTerminals = getSize(&nonTerminals);
     for (int i=0;i<numNonTerminals;i++){
+        printf("Non-terminal being considered: [%s]\n", nonTerminals.values[i]);
         set tempSet;
         tempSet.max_size = 100;
         tempSet.values = (char **)malloc(tempSet.max_size * sizeof(char *)); // some large number
-        for (int i = 0; i < tempSet.max_size ; i++) {
-            tempSet.values[i] = (char *)malloc(MAX_UNIT_SIZE * sizeof(char));
-            if (tempSet.values[i]==NULL)
+        for (int j = 0; j < tempSet.max_size ; j++) {
+            tempSet.values[j] = (char *)malloc(MAX_UNIT_SIZE * sizeof(char));
+            if (tempSet.values[j]==NULL)
             {
                 fprintf(stderr, "Failed to allocate memory for new productions' set.\n");
                 exit(4);
             }
-            tempSet.values[i][0] = '\0';
+            tempSet.values[j][0] = '\0';
         }
         for (int j=0;j<i;j++){
+            printf("  That non-terminal being considered which came before it: [%s]\n", nonTerminals.values[j]);
             for(int k=0;k<n;k++){
+                printf("    The production being considered: [%s]\n", p[k]);
                 // check if there is a production of the form 
                 // Ai -> Aj<gamma>
                 int counter = 0;
                 char *tmp1 = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
                 char *tmp2 = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
-                getToken(p[k], tmp1, counter);
-                getToken(p[k], tmp2, counter);
+                getToken(p[k], tmp1, &counter);
+                getToken(p[k], tmp2, &counter);
                 if (strcmp(tmp1, nonTerminals.values[i])==0 &&
                     strcmp(tmp2, nonTerminals.values[j])==0){
-                        
+                        printf("      It's of the required form.\n");
                         // get the remaining part of the production i.e. gamma
                         char* gamma = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
                         char *tmp4 = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
                         gamma[0] = 0;
-                        while(getToken(p[k], tmp4, counter)!=-1){
-                            strcat(gamma, " ");
+                        while(getToken(p[k], tmp4, &counter)!=-1){
+                            if (counter>3)
+                                strcat(gamma, " ");
                             strcat(gamma, tmp4);
                         }
-                        free(tmp4);
-
+                        // free(tmp4);
+                        printf("      It's extracted gamma: [%s]\n", gamma);
                         // get all Aj's productions
                         for (int l=0;l<n;l++){
                             int counter2 = 0;
-                            char *tmp3 = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
-                            getToken(p[l], tmp3, counter2);
-                            if (strcmp(tmp3, nonTerminals.values[j])==0){
+                            char *head1 = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+                            getToken(p[l], head1, &counter2);
+                            if (strcmp(head1, nonTerminals.values[j])==0){
                                 
+                                printf("        [%s]'s production being considered, to add gamma to it: [%s]\n", nonTerminals.values[j], p[l]);
                                 // append gamma at the end of the current production
-                                char *tmp5 = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
-                                strcpy(tmp5, p[l]);
-                                if (gamma[0]!=0) // if gamma is not empty add gamma
-                                    strcat(tmp5, gamma);
-                                
+                                char *toBeAdded = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+                                strcpy(toBeAdded, nonTerminals.values[i]);
+                                strcat(toBeAdded, " ");
+                                printf("         After adding head: [%s]\n", toBeAdded);
+                                char *body = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+                                getBody(body, p[l]);
+                                strcat(toBeAdded, body);
+                                printf("         After adding body: [%s]\n", toBeAdded);
+                                if (gamma[0]!=0){ // if gamma is not empty add gamma
+                                    strcat(toBeAdded, " ");
+                                    strcat(toBeAdded, gamma);
+                                    printf("         After adding gamma: [%s]\n", toBeAdded);
+                                }
+                                printf("        Production being added to the temp set for the non-terminal [%s]: [%s]\n", nonTerminals.values[i], toBeAdded);
                                 // add the new production to the new set.
-                                addElement(&tempSet, tmp5);
-                                free(tmp5);
+                                addElement(&tempSet, toBeAdded);
+                                // free(toBeAdded);
                             }
-                            free(tmp3);
+                            // free(head1);
                         }
                     }
                 else {
-                    // if not of that form, just copy it.
-                    addElement(&tempSet, p[k]);
+                    if(strcmp(tmp1, nonTerminals.values[i])==0){
+                        printf("      It's not of the required form\n");
+                        printf("      Directly adding it to the temp set for the non-terminal [%s]\n", nonTerminals.values[i]);
+                        // if not of that form, just copy it.
+                        addElement(&tempSet, p[k]);
+                    }
                 }
-
-
-
-                free(tmp1);
-                free(tmp2);
+                // free(tmp1);
+                // free(tmp2);
             }
         }
+
+        if (i==0){
+            // add all those productions which were there in Ai originally
+            printf("  Adding its productions that were present originally, to its temp set\n");
+            for(int j=0;j<n;j++){
+                char *head = (char*)malloc(MAX_UNIT_SIZE*sizeof(char));
+                getHead(head, p[j]);
+                if(strcmp(head, nonTerminals.values[i])==0){
+                    printf("    Adding [%s]\n", p[j]);
+                    addElement(&tempSet, p[j]);
+                }
+                // free(head);
+            }
+        }
+
         // remove immediate left recursion among the Ai productions.
         // then add the final thing to the new set of productions.
 
         addSet(&prods1, cleanImmLR(&tempSet));
-    
-        for (int i = 0; i < tempSet.max_size ; i++)
-            free(tempSet.values[i]);
 
-        free(tempSet.values);
+        // for (int i = 0; i < tempSet.max_size ; i++)
+        //     free(tempSet.values[i]);
+
+        // free(tempSet.values);
     }
+    printf("The LR removed grammar: \n");
+    printSet(&prods1);
 }
