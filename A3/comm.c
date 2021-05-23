f4fbfa4c0d5ae0e76c9389cbe52a2282c40f9e80
garg-7 <garg.7@iitj.ac.in>
2021-05-23 09:02:48 +0530

    added test file for comment removal

---
+++ A3/comm.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+// jjf iyvoug o gg o      /*
+int main(/* eobcwo cbwp */)
+{
+    char ch[] = "/s0329ry3h /8e l/* e cowe c \
+    \
+    doc ///// / / / /we iew \
+    \
+     \
+     we\
+     */ lc wel "; /*hc owe oweb pwe w*
+
+     eljhw eovwebp ipew 
+     v w w
+     v*/;
+}
+
+///////////////////
